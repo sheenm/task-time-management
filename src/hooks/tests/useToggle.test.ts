35d9d91bd9dd1c61c51e84eb11f917e809702201
SheenM <sheenm@ya.ru>
2019-04-11 22:19:36 +1000

    added react-testing-library + useToggleTests

---
+++ src/hooks/tests/useToggle.test.ts
@@ -0,0 +1,36 @@
+import { act, renderHook } from 'react-hooks-testing-library'
+import { useToggle } from '../useToggle'
+
+describe('UseToggle.test', () => {
+  test('should change state from false to true', () => {
+    const { result } = renderHook(() => useToggle(false))
+
+    const toggle = result.current[1]
+    act(toggle)
+
+    const state = result.current[0]
+    expect(state).toBeTruthy()
+  })
+
+  test('should change state from true to false', () => {
+    const { result } = renderHook(() => useToggle(true))
+
+    const toggle = result.current[1]
+    act(toggle)
+
+    const state = result.current[0]
+    expect(state).toBeFalsy()
+  })
+
+  test('should hold the same state after 2 iterations', async () => {
+    const { result } = renderHook(() => useToggle(true))
+
+    const toggleFirstTime = result.current[1]
+    act(toggleFirstTime)
+    const toggleSecondTime = result.current[1]
+    act(toggleSecondTime)
+
+    const state = result.current[0]
+    expect(state).toBeTruthy()
+  })
+})
--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -3,5 +3,8 @@ import React from 'react'
 export function useToggle(initialState: boolean): [boolean, () => void] {
   const [isOn, setIsOn] = React.useState(initialState)
 
-  return [isOn, () => setIsOn(!isOn)]
+  const setOn = React.useCallback(() => setIsOn(true), [])
+  const setOff = React.useCallback(() => setIsOn(false), [])
+
+  return [isOn, isOn ? setOff : setOn]
 }
