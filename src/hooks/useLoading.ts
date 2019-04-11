7977480107b38cc3574de041c92682ec690cd615
SheenM <sheenm@ya.ru>
2019-04-12 00:26:56 +1000

    comments to hooks

--- src/hooks/useLoading.ts
+++ src/hooks/useLoading.ts
@@ -1,5 +1,10 @@
 import React from 'react'
 
+/**
+ * React hook which loads data and provides isLoading state value
+ * @param load what to await before component is loaded
+ * @param then what to do when the component is loaded
+ */
 export function useLoading<T>(load: () => Promise<T>, then: (info: T) => void) {
   const [isLoading, setIsLoading] = React.useState(false)
 
--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,5 +1,9 @@
 import React from 'react'
 
+/**
+ * Provides state (On/Off) and toggle function to a component
+ * @param initialState with which state will it be initialized
+ */
 export function useToggle(initialState: boolean): [boolean, () => void] {
   const [isOn, setIsOn] = React.useState(initialState)
 
