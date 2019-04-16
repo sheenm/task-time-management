6b5de58b4e3b2f91323251aa87708367366f4317
SheenM <sheenm@ya.ru>
2019-04-16 22:13:20 +1000

    moved timestamps logic to Task for proper update

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,8 +1,61 @@
-import { ITask } from 'app/dto'
-import React from 'react'
+import { ITask, ITimestamp } from 'app/dto'
+import React, { useReducer } from 'react'
+import { LoadingStastes, useLoading } from '../../hooks/useLoading'
 import { useToggle } from '../../hooks/useToggle'
-import { Timestamps } from '../timestamp/Timestamps'
+import { neverReached } from '../../utils/neverReached'
+import { RepositoryContext } from '../repositories/RepositoryContext'
+import { Timestamp } from '../timestamp/Timestamp'
 import { TaskPresenter } from './TaskPresenter'
+
+interface ILoadTimestampsAction {
+  type: 'LOAD_TIMESTAMPS'
+  timestamps: ITimestamp[]
+}
+
+interface IRemoveTimestampAction {
+  type: 'REMOVE_TIMESTAMP'
+  id: number
+}
+
+interface IRenameTimestampAction {
+  type: 'CHANGE_TIMESTAMP_COMMENT'
+  id: number
+  newComment: string
+}
+
+interface ICreateTimestampAction {
+  type: 'CREATE_TIMESTAMP',
+  timestamp: ITimestamp
+}
+
+type ActionTypes = ILoadTimestampsAction |
+  IRemoveTimestampAction |
+  IRenameTimestampAction |
+  ICreateTimestampAction
+
+const reducer = (state: ITimestamp[], action: ActionTypes) => {
+
+  switch (action.type) {
+    case 'LOAD_TIMESTAMPS':
+      return action.timestamps
+    case 'REMOVE_TIMESTAMP':
+      const removedIndex = state.findIndex(x => x.id === action.id)
+      const removedState = [...state]
+      removedState.splice(removedIndex, 1)
+
+      return removedState
+    case 'CHANGE_TIMESTAMP_COMMENT':
+      const renamedState = [...state]
+      const renamedIndex = state.findIndex(x => x.id === action.id)
+      renamedState[renamedIndex].comment = action.newComment
+
+      return renamedState
+    case 'CREATE_TIMESTAMP':
+      return [...state, action.timestamp]
+    default:
+      return neverReached(action)
+  }
+}
 
 interface IProps {
   task: ITask
@@ -11,8 +64,28 @@ interface IProps {
 
 export const Task: React.FC<IProps> = ({ task, rename }) => {
   const [isOpen, toggleOpen] = useToggle(false)
+  const [stateTimestamps, dispatch] = useReducer(reducer, [])
+  const { timestampsRepo } = React.useContext(RepositoryContext)
+
+  const loadingState = useLoading(() => timestampsRepo.get(task.id))
+    (timestamps => dispatch({ type: 'LOAD_TIMESTAMPS', timestamps }))
+
+  const createChangeCommentFn = (timestamp: ITimestamp) =>
+    (newComment: string) => {
+      const changedTimestamp: ITimestamp = { ...timestamp, comment: newComment }
+      timestampsRepo.save(changedTimestamp)
+        .then(() => dispatch({ type: 'CHANGE_TIMESTAMP_COMMENT', id: timestamp.id, newComment }))
+    }
+
+  const createRemoveFn = (id: number) =>
+    () => timestampsRepo.delete(id)
+      .then(() => dispatch({ type: 'REMOVE_TIMESTAMP', id }))
+
   const toggleTaskStart = React.useCallback(() =>
     console.log('todo 2: Can Start and Stop tasks'), [])
+
+  if (loadingState === LoadingStastes.Loading)
+    return <h1>todo loading 10. Data loading trobber</h1>
 
   return <TaskPresenter
     title={task.title}
@@ -22,6 +95,12 @@ export const Task: React.FC<IProps> = ({ task, rename }) => {
     isStarted={false}
     toggleTaskStart={toggleTaskStart}
   >
-    <Timestamps taskId={task.id} />
+    {stateTimestamps.map(x =>
+      <Timestamp
+        key={x.id}
+        timestamp={x}
+        changeComment={createChangeCommentFn(x)}
+        remove={createRemoveFn(x.id)} />
+    )}
   </TaskPresenter>
 }
