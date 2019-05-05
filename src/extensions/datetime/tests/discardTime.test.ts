84f3107c4868ff2a8211d25493ec9a54a091c2b8
SheenM <sheenm@ya.ru>
2019-05-06 00:59:06 +1000

    tslint fix

--- src/extensions/datetime/tests/discardTime.test.ts
+++ src/extensions/datetime/tests/discardTime.test.ts
@@ -1,8 +1,9 @@
 import '../discardTime'
 
+// tslint:disable: no-magic-numbers
 describe('DiscardTime.test', () => {
   it('should discard time from date', () => {
-    // tslint:disable-next-line: no-magic-numbers
+
     const date = new Date(2019, 5, 1, 5, 2, 2, 25)
 
     expect(date.extDiscardTime()).toEqual(new Date(2019, 5, 1))
