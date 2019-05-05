3f7f9314e45494148333f44129d192bab40c7d96
SheenM <sheenm@ya.ru>
2019-05-05 15:45:37 +1000

    DateTime class to extensions

--- src/components/report/ReportSettingsPicker/standardPeriods.ts
+++ src/components/report/ReportSettingsPicker/standardPeriods.ts
@@ -1,16 +1,18 @@
 import { ITimestamp } from "app/dto"
 import { IStandardPeriods } from "app/report"
-import { Datetime } from "../../../utils/datetime"
+import '../../../extensions/datetime/addDays'
+import '../../../extensions/datetime/discardTime'
+import '../../../extensions/datetime/isEqual'
 
 export const standardPeriods: IStandardPeriods = {
   today: {
     title: 'Today',
-    filterFunction: (timestamps) => filterByDay(new Datetime(), timestamps)
+    filterFunction: (timestamps) => filterByDay(new Date(), timestamps)
   },
   yesterday: {
     title: 'Yesterday',
     filterFunction: (timestamps) => {
-      const yesterday = new Datetime().addDays(-1)
+      const yesterday = new Date().extAddDays(-1)
 
       return filterByDay(yesterday, timestamps)
     }
@@ -20,42 +22,42 @@ export const standardPeriods: IStandardPeriods = {
     filterFunction: (timestamps) => {
 
       let lastTimestampBesideTodays: ITimestamp | undefined
-      const today = new Datetime()
+      const today = new Date()
 
       for (const timestamp of timestamps) {
-        const currentDateStart = new Datetime(timestamp.datetimeStart)
+        const currentDateStart = new Date(timestamp.datetimeStart)
 
-        if (today.isDayEqual(currentDateStart))
+        if (today.extIsDayEqual(currentDateStart))
           continue
 
         if (lastTimestampBesideTodays === undefined)
           lastTimestampBesideTodays = timestamp
 
-        const lastDateStart = new Datetime(lastTimestampBesideTodays.datetimeStart)
-        if (currentDateStart.isDayEqualOrGreater(lastDateStart))
+        const lastDateStart = new Date(lastTimestampBesideTodays.datetimeStart)
+        if (currentDateStart.extIsDayEqualOrGreater(lastDateStart))
           lastTimestampBesideTodays = timestamp
       }
 
       if (lastTimestampBesideTodays === undefined)
         return []
 
-      const lastDatetimeStart = new Datetime(lastTimestampBesideTodays.datetimeStart)
+      const lastDatetimeStart = new Date(lastTimestampBesideTodays.datetimeStart)
 
       return filterByDay(lastDatetimeStart, timestamps)
     }
   },
 }
 
-function filterByDay(day: Datetime, timestamps: ITimestamp[]) {
+function filterByDay(day: Date, timestamps: ITimestamp[]) {
   return timestamps
     .filter(x => filter(day, x))
 }
 
-function filter(day: Datetime, timestamp: ITimestamp) {
-  const dateStart = new Datetime(timestamp.datetimeStart)
-    .discardTime()
+function filter(day: Date, timestamp: ITimestamp) {
+  const dateStart = new Date(timestamp.datetimeStart)
+    .extDiscardTime()
   // if datetimeEnd is undefined it will give you now
-  const dateEnd = new Datetime(timestamp.datetimeEnd)
+  const dateEnd = new Date(timestamp.datetimeEnd || new Date())
 
-  return day.isDayEqualOrGreater(dateStart) && dateEnd.isDayEqualOrGreater(day)
+  return day.extIsDayEqualOrGreater(dateStart) && dateEnd.extIsDayEqualOrGreater(day)
 }
---
+++ src/extensions/datetime/discardTime.ts
@@ -0,0 +1,21 @@
+declare global {
+  // tslint:disable-next-line: interface-name
+  interface Date {
+    /**
+     * Sets time to 0 0 0 0 to get clear date
+     */
+    extDiscardTime: () => Date
+  }
+}
+
+if (Date.prototype.extDiscardTime === undefined) {
+  Date.prototype.extDiscardTime = function () {
+    // tslint:disable-next-line: no-invalid-this
+    const date = new Date(this)
+    date.setHours(0, 0, 0, 0)
+
+    return date
+  }
+}
+
+export { }
